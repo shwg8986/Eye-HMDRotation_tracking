d17048ed271a873386efbc5120b3e6aa126762c7
shwg <67375194+shwg8986@users.noreply.github.com>
2022-06-22 11:37:51 +0900

    Update VIVEsample.cs

--- VIVEsample.cs
+++ VIVEsample.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
@@ -10,19 +10,18 @@ namespace ViveSR
     {
         namespace Eye
         {
-
             public class VIVEsample : MonoBehaviour
             {
                 //csv
-                public string filename = "eyeInfo_roaller";
+                public string filename = "EyeTracking";
                 StreamWriter sw;
 
-                //⓪取得呼び出し-----------------------------
+                //vive pro eyeから眼球情報呼び出し-----------------------------
                 //呼び出したデータ格納用の関数
                 EyeData eye;
                 //-------------------------------------------
 
-                //①瞳孔位置--------------------
+                //瞳孔位置--------------------
                 //x,y軸
                 //左の瞳孔位置格納用関数
                 Vector2 LeftPupil;
@@ -30,13 +29,13 @@ namespace ViveSR
                 Vector2 RightPupil;
                 //------------------------------
 
-                //④瞳孔の直径-------------------------------
+                //瞳孔の直径-------------------------------
                 //呼び出したデータ格納用の関数
                 float LeftPupiltDiameter;
                 float RightPupiltDiameter;
                 //-------------------------------------------
 
-                //②まぶたの開き具合------------
+                //まぶたの開き具合------------
                 //左のまぶたの開き具合格納用関数
                 float LeftBlink;
                 //右のまぶたの開き具合格納用関数
@@ -44,7 +43,7 @@ namespace ViveSR
                 //------------------------------
 
 
-                //③視線情報--------------------
+                //視線情報--------------------
                 //origin：起点，direction：レイの方向　x,y,z軸
                 //両目の視線格納変数
                 Vector3 CombineGazeRayorigin;
@@ -57,23 +56,20 @@ namespace ViveSR
                 Vector3 RightGazeRaydirection;
                 //------------------------------
 
-                //④焦点情報--------------------
+                //焦点情報--------------------
                 //両目の焦点格納変数
-                //レイの始点と方向（多分③の内容と同じ）
                 Ray CombineRay;
-                /*レイがどこに焦点を合わせたかの情報．Vector3 point : 視線ベクトルと物体の衝突位置，float distance : 見ている物体までの距離，
-                   Vector3 normal:見ている物体の面の法線ベクトル，Collider collider : 衝突したオブジェクトのCollider，Rigidbody rigidbody：衝突したオブジェクトのRigidbody，Transform transform：衝突したオブジェクトのTransform*/
-                //焦点位置にオブジェクトを出すためにpublicにしています．
+                
                 public static FocusInfo CombineFocus;
                 //レイの半径
                 float CombineFocusradius;
                 //レイの最大の長さ
                 float CombineFocusmaxDistance;
-                //オブジェクトを選択的に無視するために使用されるレイヤー ID
+                //オブジェクトを選択的に無視するために使用されるレイヤー
                 int CombinefocusableLayer = 0;
                 //------------------------------
 
-                //③輻輳距離
+                //輻輳距離(まだ実装されていないので常に値が0)
                 float convergence_distance;
 
                 //パッドのどこを触っているのかを取得するためのTrackPadという関数にSteamVR_Actions.default_TrackPadを固定
@@ -85,17 +81,9 @@ namespace ViveSR
                 //どこのcontrollerの領域に触れていたかを格納
                 string position;
 
-                //時間格納
+                //時間とFPS格納
                 float Time_;
                 float FPS;
-
-                /*
-                public void Awake()
-                {
-                    QualitySettings.vSyncCount = 0;
-                    Application.targetFrameRate = 20; //60FPSに設定
-                }
-                */
 
                 void Start()
                 {
@@ -108,30 +96,21 @@ namespace ViveSR
 
                     string s2 = string.Join(",", s1);
                     sw.WriteLine(s2);
-
-                   
                 }
 
-                //1フレーム毎に実行
                 void FixedUpdate()
                 {
-
+                    
+                    //時間の取得
                     Time_ = UnityEngine.Time.time;
-                    //おまけ------------------------------------
-                    //エラー確認ViveSR.Error.がWORKなら正常に動いている．（フレームワークの方に内蔵済みだからいらないかも）
-                    if (SRanipal_Eye_API.GetEyeData(ref eye) == ViveSR.Error.WORK)
-                    {
-                        //一応機器が正常に動いてる時の処理をここにかける
-                    }
-                    //-------------------------------------------
 
 
-                    //⓪取得呼び出し-----------------------------
+                    //取得呼び出し-----------------------------
                     SRanipal_Eye_API.GetEyeData(ref eye);
                     //-------------------------------------------
 
 
-                    //①瞳孔位置---------------------（HMDを被ると検知される，目をつぶっても位置は返すが，HMDを外すとと止まる．目をつぶってるときはどこの値返してんのか謎．一応まぶた貫通してるっぽい？？？）
+                    //瞳孔位置---------------------
                     //左の瞳孔位置を取得
                     if (SRanipal_Eye.GetPupilPosition(EyeIndex.LEFT, out LeftPupil))
                     {
@@ -146,15 +125,15 @@ namespace ViveSR
                     }
                     //------------------------------
 
-                    //④瞳孔の直径-------------------------------
-                    //左目の瞳孔の直径が妥当ならば取得　目をつぶるとFalse 判定精度はまあまあ
+                    //瞳孔の直径-------------------------------
+                    //左目の瞳孔の直径が妥当ならば取得　目をつぶるとFalse
                     if (eye.verbose_data.left.GetValidity(SingleEyeDataValidity.SINGLE_EYE_DATA_PUPIL_DIAMETER_VALIDITY))
                     {
                         LeftPupiltDiameter = eye.verbose_data.left.pupil_diameter_mm;
                         //Debug.Log("Left Pupilt Diameter：" + LeftPupiltDiameter);
                     }
 
-                    ////右目の瞳孔の直径が妥当ならば取得　目をつぶるとFalse　判定精度はまあまあ
+                    ////右目の瞳孔の直径が妥当ならば取得　目をつぶるとFalse
                     if (eye.verbose_data.right.GetValidity(SingleEyeDataValidity.SINGLE_EYE_DATA_PUPIL_DIAMETER_VALIDITY))
                     {
                         RightPupiltDiameter = eye.verbose_data.right.pupil_diameter_mm;
@@ -164,7 +143,7 @@ namespace ViveSR
 
 
 
-                    //②まぶたの開き具合------------（HMDを被ってなくても1が返ってくる？？謎）
+                    //まぶたの開き具合
                     //左のまぶたの開き具合を取得
                     if (SRanipal_Eye.GetEyeOpenness(EyeIndex.LEFT, out LeftBlink, eye))
                     {
@@ -181,7 +160,7 @@ namespace ViveSR
 
 
 
-                    //③視線情報--------------------（目をつぶると検知されない）
+                    //視線情報--------------------
                     //両目の視線情報が有効なら視線情報を表示origin：起点，direction：レイの方向
                     if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out CombineGazeRayorigin, out CombineGazeRaydirection, eye))
                     {
@@ -205,7 +184,7 @@ namespace ViveSR
                     }
                     //------------------------------
 
-                    //④焦点情報--------------------
+                    //焦点情報--------------------
                     //radius, maxDistance，CombinefocusableLayerは省略可
                     if (SRanipal_Eye.Focus(GazeIndex.COMBINE, out CombineRay, out CombineFocus/*, CombineFocusradius, CombineFocusmaxDistance, CombinefocusableLayer*/))
                     {
@@ -214,11 +193,12 @@ namespace ViveSR
                     //------------------------------
 
 
-                    //⑤輻輳距離なぜか取得できない...
+                    //輻輳距離（取得できない）
                     convergence_distance = eye.verbose_data.combined.convergence_distance_mm;
 
 
-                    //TrackPad
+                    //ViveコントローラTrackPadの触れている位置を取得する
+                    
                     //結果をGetLastAxisで取得してposrightに格納
                     //SteamVR_Input_Sources.機器名（ここは右コントローラ）
                     posright = TrackPad.GetLastAxis(SteamVR_Input_Sources.RightHand);
@@ -252,9 +232,9 @@ namespace ViveSR
                         }
                     }
 
-                    //Debug.Log(posright.x + " " + posright.y);
-                    Debug.Log("position : " + position);
-
+                    //Debug.Log("position : " + position);
+                    
+                    //FPSをlogで確認する
                     FPS = 1 / UnityEngine.Time.deltaTime;
                     Debug.Log("FPS : " + FPS);
 
@@ -263,12 +243,10 @@ namespace ViveSR
                         CombineFocus.point.x,"" + CombineFocus.point.y,"" + CombineFocus.point.z,""+ posright.x,""+posright.y,""+position,""+
                         Time_,""+ FPS};
                     string str2 = string.Join(",", str);
-                    
-                    //Debug.Log(str2);
 
                     sw.WriteLine(str2);
 
-
+                    //ファイルを閉じる
                     if (Input.GetKeyDown(KeyCode.F))
                     {
                         sw.Close();
